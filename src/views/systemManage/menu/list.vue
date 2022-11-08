64c7c057c753a4774b96850f812290ca823d60c6
CHENXIAOXIN\83745 <873456296@qq.com>
2022-11-08 17:30:22 +0800

    删除菜单

--- src/views/systemManage/menu/list.vue
+++ src/views/systemManage/menu/list.vue
@@ -35,7 +35,7 @@
       >
       <el-table-column fixed="right" align="center" label="操作" width="200" >
         <template slot-scope="scope">
-            <el-button type="text" @click="addDialogList(scope.row)" >新增下级</el-button>
+            <el-button type="text" @click="addDialogList(scope.row)" >新增菜单</el-button>
             <el-button type="text" @click="updateDialog(scope.row)" >修改</el-button>
             <el-button type="text" @click="goDel(scope.row)">删除</el-button>
             <el-button type="text" @click="goCopy(scope.row)">复制</el-button>
